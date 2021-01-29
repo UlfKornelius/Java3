0598fbf61d102bb19225a9ba69f5ce0ae1d2b887
ulfkornelius <wushu.kgd@mail.ru>
2021-01-29 13:19:26 +0200

    HW №4

---
+++ src/java3/lesson4/HW4.java
@@ -0,0 +1,65 @@
+package ru.bondarev.java3.lesson4;
+
+public class HW4 {
+
+    volatile int status = 0;
+    public static void main(String[] args) {
+        HW4 lock = new HW4();
+        Thread t1 = new Thread(new Task("A", lock,1));
+        Thread t2 = new Thread(new Task("B", lock,2));
+        Thread t3 = new Thread(new Task("C", lock,3));
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
+
+class Task implements Runnable {
+
+    private String message;
+    private final HW4 lock;
+    private int p;
+
+    Task(String text, HW4 obj, int p) {
+        message = text;
+        this.lock = obj;
+        this.p = p;
+    }
+
+    @Override
+    public void run() {
+
+        while(lock.status < 13) {
+            synchronized (lock) {
+
+                while(!((lock.status % 3) == 0) && p == 1){
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                while(!((lock.status % 3) == 1) && p == 2){
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                while(!((lock.status % 3) == 2) && p == 3){
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                System.out.println(message);
+                lock.status++;
+                if ((lock.status % 3) == 0) System.out.println("---");
+                lock.notifyAll();
+            }
+        }
+    }
+}
